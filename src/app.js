13c53c706014b63d6ab22ac2dc7ab3fd76398b3a
VinodVhaash <vinodpatil2036@gmail.com>
2025-01-28 13:56:13 +0530

    Basics of roting

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.use("/hello/about/do", (req, res) => {
+  res.send("do from the server...");
+});
+
+app.use("/hello/about", (req, res) => {
+  res.send("about from the server...");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("hello from the server...");
+});
+
 app.use("/test", (req, res) => {
   res.send("Test from the server...");
 });
 
+app.use("/", (req, res) => {
+  res.send("slash from the server...");
+});
+
 app.use((req, res) => {
-  res.send("Hello from the server...");
+  res.send("Hiiiiiiiii from the server...");
 });
 
 app.listen(3000, () => {
