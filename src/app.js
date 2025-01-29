4af76a8949e365dc8007d0ac3697c64740246091
VinodVhaash <vinodpatil2036@gmail.com>
2025-01-29 12:06:20 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,29 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello/about/do", (req, res) => {
-  res.send("do from the server...");
-});
-
-app.use("/hello/about", (req, res) => {
-  res.send("about from the server...");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello from the server...");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Test from the server...");
-});
-
-// app.use("/", (req, res) => {
-//   res.send("slash from the server...");
-// });
-
-// app.use((req, res) => {
-//   res.send("Hiiiiiiiii from the server...");
-// });
+app.use(
+  "/user",
+  (req, res, next) => {
+    //route handler
+    //res.send("route handler 1");
+    next();
+  },
+  (req, res) => {
+    res.send("route handler 2");
+  }
+);
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000.....`);
