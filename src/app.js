7605b8cacd0a36e221b4fbecde563b8b68127d8d
VinodVhaash <vinodpatil2036@gmail.com>
2025-01-31 00:52:10 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth } = require("./middlewares/auth");
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data send...");
+app.get("/getUserData", (req, res) => {
+  //error hadling using try catch
+  try {
+    throw new Error("dsdfsf");
+    res.send("User data sent");
+  } catch (err) {
+    res.status(500).send("Some error occured...contact support team.");
+  }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("User Deleted...");
+//error handler.
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong.");
+  }
 });
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000.....`);
